5e013a4c84ea287c7949a73e6e7e094532d20de4
Gimota <drgislenemota@gmail.com>
2024-10-20 15:09:24 -0300

    criacao de scripts sql da pagina index

---
+++ db/consultas index.sql
@@ -0,0 +1,7 @@
+
+--criacao de consulta de gategorias que serao exibidas no index
+select * from CATEGORIAS
+
+--criacao de consulta de gategorias que serao exibidas no index (caso crie uma coluna de sugestao do chef, 
+--podemos colocar um filtro a mais sendo where sugestao_chef = true)
+select * from PRATOS 
