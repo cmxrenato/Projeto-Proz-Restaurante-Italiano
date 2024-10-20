fdd0aab9c689908c916cdc5a5eb38e36518c38dd
Gimota <drgislenemota@gmail.com>
2024-10-20 15:15:56 -0300

    alteracao scripts sql da pagina index para considerar nova coluna de sugestao_chef

--- db/consultas_index.sql
+++ db/consultas_index.sql
@@ -2,6 +2,5 @@
 --criacao de consulta de gategorias que serao exibidas no index
 select * from CATEGORIAS
 
---criacao de consulta de gategorias que serao exibidas no index (caso crie uma coluna de sugestao do chef, 
---podemos colocar um filtro a mais sendo where sugestao_chef = true)
-select * from PRATOS 
+--criacao de consulta de gategorias que serao exibidas no index (caso crie uma coluna de sugestao do chef
+select * from PRATOS where sugestao_chef = 1
