6b88739ce1e33588fd9767065835563523dc2650
cmxrenato <renatoaraujoleal@gmail.com>
2024-10-21 20:20:26 -0300

    sugestao chef true

--- db/consultas_index.sql
+++ db/consultas_index.sql
@@ -3,4 +3,4 @@
 select * from CATEGORIAS
 
 --criacao de consulta de gategorias que serao exibidas no index (caso crie uma coluna de sugestao do chef
-select * from PRATOS where sugestao_chef = 1
+select * from PRATOS where sugestao_chef = TRUES
